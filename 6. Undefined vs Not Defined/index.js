02ae1f6eecd3620b231228d62df25930ab247ab7
tariq7R1X <tariquedahri04@gmail.com>
2025-07-19 18:31:29 +0500

    typeof avoids reference errors

--- 6. Undefined vs Not Defined/index.js
+++ 6. Undefined vs Not Defined/index.js
@@ -1,7 +1,12 @@
 
 
 
-let a;
+let a=10;
 console.log(a);
 
 console.log(b);
+
+// ✅ This works safely even if y is not declared, because typeof avoids ReferenceError.
+if (typeof y !== "undefined") {
+  console.log("y is declared");
+}
